dc3adefd20c000a80614dcef194248766f638502
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-03-08 17:03:24 -0400

    Update app.js

---
+++ design/app.js
@@ -0,0 +1,17 @@
+const thankCard = document.querySelector(".main__card-thank");
+const ratingCard = document.querySelector(".main__card-rating");
+const submit = document.querySelector(".main__card-submit");
+const buttons = document.querySelectorAll(".main__card-review-button");
+const span = document.querySelector(".main__card-info-rated");
+
+const handleSubmitButtonClick = () => {
+    thankCard.classList.add("main__card-thank--active");
+    ratingCard.classList.add("main__card-rating--not-active");
+}
+
+const handleReviewButtonClick = e => {
+    span.textContent = parseInt(e.currentTarget.textContent);
+}
+
+submit.addEventListener('click', handleSubmitButtonClick);
+buttons.forEach(button => button.addEventListener('click', handleReviewButtonClick));
